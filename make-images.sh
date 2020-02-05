4ca54fc2ebba689418b753f0c9531b6c840f25f5
Vivek Rai <mail@raivivek.in>
2020-02-05 01:45:45 -0500

    Add demuxlet to core
    Signed-off-by: Vivek Rai <mail@raivivek.in>

--- make-images.sh
+++ make-images.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
+# TODO: versions for each definition file (update before deploying)
+
 for tag in atac rna core singlecell; do
   # building on macOS
-  singularity build --remote $tag/$tag.sif $tag/Singularity.$tag &> $tag/$tag.log &&
-    singularity push $tag/$tag.sif "library://raivivek/default/bio-images:$tag"
+  echo "singularity build --remote $tag/$tag.sif $tag/Singularity.$tag &> $tag/$tag.log &&
+    singularity push $tag/$tag.sif "library://raivivek/default/bio-images:$tag""
 done
